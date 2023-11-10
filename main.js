2b8b101016de8770764857a8bf7fe1dbf0363f98
darellanodev <darellano.aliaga@gmail.com>
2023-11-10 19:56:35 +0100

    load images and display hud

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let myTower
 
 const TOTAL_TOWER_UPGRADES = 5
 const TOTAL_ENEMIES = 5
+const CANVAS_WIDTH = 815
+const CANVAS_HEIGHT = 650
+const HUD_HEIGHT = 84
 
 function preload() {
 
@@ -22,31 +25,42 @@ function preload() {
         loadImage('img/floor/gray.png'),
     ]
 
+    backgroundImage = loadImage('img/backgrounds/ground.jpg')
+    hudImages = [
+        loadImage('img/hud/normal.png'),
+        loadImage('img/hud/upgrading.png'),
+        loadImage('img/hud/upgrading_max.png'),
+    ]
     
 }
 
 function setup() {
-    createCanvas(900, 700)
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
 
     const levelMap = '111111111111111x,1000000000000000,1011111111111111,1010000000000001,1010000111111101,1011111100000101,1000000000000101,1111111111111101,0000000000000001,y111111111111111'
 
-    myGrid = new Grid(levelMap, floorImages)
-    myEnemy = new Enemy(enemiesImages[0])
-    myTower = new Tower(tower1Images)
-
-
+    grid = new Grid(levelMap, floorImages)
+    enemy = new Enemy(enemiesImages[0])
+    tower = new Tower(tower1Images)
+    hud = new Hud(hudImages)
 }
 
 function draw() {
     background('skyblue')
     rectMode(CORNER)
 
-    myGrid.draw()
-    myEnemy.draw()
-    myTower.draw()
+    image(backgroundImage, 0, HUD_HEIGHT)
+    
+    grid.draw()
+    enemy.draw()
+    tower.draw()
+
+    hud.draw()
+    
+
 
     // HUD
     textSize(20)
     fill('yellow')
-    text('This is a sample text', 50, 50)
+    text('This is a sample text', 120, 20)
 }
