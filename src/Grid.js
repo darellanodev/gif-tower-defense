16935087c121eea8350aaee888f3cba89d715108
darellanodev <darellano.aliaga@gmail.com>
2023-11-10 19:55:30 +0100

    Set a margin top and refactor coordinates

--- src/Grid.js
+++ src/Grid.js
@@ -1,6 +1,7 @@
 class Grid {
 
     FLOOR_SIZE = 50
+    MARGIN_TOP = 30
 
     constructor(levelMap, mapImages) {
         this.levelMap = levelMap
@@ -15,11 +16,15 @@ class Grid {
         mapArray.forEach((element) => {
             row++
             for (let column = 0; column < element.length; column++) {
-                const character = element[column];
+                const character = element[column]
+                const posX = column + (this.FLOOR_SIZE * column)
+                const posY = row + (this.FLOOR_SIZE * row) + this.MARGIN_TOP
                 if (character == '0') {
-                    image(this.orangeImage, column + (this.FLOOR_SIZE * column), row + (this.FLOOR_SIZE * row));
+                    image(this.orangeImage, posX, posY);
                 } else if (character == '1') {
-                    image(this.blackImage, column + (this.FLOOR_SIZE * column), row + (this.FLOOR_SIZE * row));
+                    // empty
+                } else if (character == '2') {
+                    image(this.blackImage, posX, posY);
                 }
             }
         });
