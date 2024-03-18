04f8f21343c4692d53ccdd3acfc646ce60533ec8
darellanodev <darellano.aliaga@gmail.com>
2024-03-18 20:42:16 +0100

    tests for Hud.isInsideMagicsButtonsBar

--- test/Hud.test.ts
+++ test/Hud.test.ts
@@ -18,3 +18,22 @@ describe('isInsideTowersButtonsBar', () => {
     expect(result).toBeFalsy
   })
 })
+
+describe('isInsideMagicsButtonsBar', () => {
+  test('when mouse is inside, return true', () => {
+    const posMouseX = 560
+    const posMouseY = 56
+
+    const result = Hud.isInsideTowersButtonsBar(posMouseX, posMouseY)
+
+    expect(result).toBeTruthy
+  })
+  test('when mouse is outside, return false', () => {
+    const posMouseX = 235
+    const posMouseY = 56
+
+    const result = Hud.isInsideTowersButtonsBar(posMouseX, posMouseY)
+
+    expect(result).toBeFalsy
+  })
+})
