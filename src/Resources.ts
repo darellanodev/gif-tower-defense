125866266ddb3cee38e0fd989ffb26c19d2d379c
darellanodev <darellano.aliaga@gmail.com>
2024-04-06 19:22:21 +0200

    change path

--- src/Resources.ts
+++ src/Resources.ts
@@ -8,10 +8,10 @@ export class Resources {
     const enemiesImages: any[] = []
     const countEnemiesAndBoss = Enemy.TOTAL_ENEMIES + 1
     MathUtils.range(1, countEnemiesAndBoss).forEach((v) => {
-      enemiesImages.push(P5.p5.loadImage('img/enemies/' + v + '_center.png'))
-      enemiesImages.push(P5.p5.loadImage('img/enemies/' + v + '_left.png'))
-      enemiesImages.push(P5.p5.loadImage('img/enemies/' + v + '_right.png'))
-      enemiesImages.push(P5.p5.loadImage('img/enemies/' + v + '_closed.png'))
+      enemiesImages.push(P5.p5.loadImage('../img/enemies/' + v + '_center.png'))
+      enemiesImages.push(P5.p5.loadImage('../img/enemies/' + v + '_left.png'))
+      enemiesImages.push(P5.p5.loadImage('../img/enemies/' + v + '_right.png'))
+      enemiesImages.push(P5.p5.loadImage('../img/enemies/' + v + '_closed.png'))
     })
     return enemiesImages
   }
@@ -20,7 +20,7 @@ export class Resources {
     const greenTowerImages: any[] = []
     MathUtils.range(0, Const.UPGRADE_MAX_LEVEL).forEach((v) => {
       greenTowerImages.push(
-        P5.p5.loadImage('img/towers/green_tower_upgrade_' + v + '.png'),
+        P5.p5.loadImage('../img/towers/green_tower_upgrade_' + v + '.png'),
       )
     })
     return greenTowerImages
@@ -30,7 +30,7 @@ export class Resources {
     const redTowerImages: any[] = []
     MathUtils.range(0, Const.UPGRADE_MAX_LEVEL).forEach((v) => {
       redTowerImages.push(
-        P5.p5.loadImage('img/towers/red_tower_upgrade_' + v + '.png'),
+        P5.p5.loadImage('../img/towers/red_tower_upgrade_' + v + '.png'),
       )
     })
     return redTowerImages
@@ -40,7 +40,7 @@ export class Resources {
     const yellowTowerImages: any[] = []
     MathUtils.range(0, Const.UPGRADE_MAX_LEVEL).forEach((v) => {
       yellowTowerImages.push(
-        P5.p5.loadImage('img/towers/yellow_tower_upgrade_' + v + '.png'),
+        P5.p5.loadImage('../img/towers/yellow_tower_upgrade_' + v + '.png'),
       )
     })
     return yellowTowerImages
@@ -48,52 +48,52 @@ export class Resources {
 
   static tileImages() {
     return [
-      P5.p5.loadImage('img/tiles/orange.png'),
-      P5.p5.loadImage('img/tiles/black.png'),
-      P5.p5.loadImage('img/tiles/end_down.png'),
-      P5.p5.loadImage('img/tiles/end_right.png'),
-      P5.p5.loadImage('img/tiles/end_left.png'),
-      P5.p5.loadImage('img/tiles/end_up.png'),
-      P5.p5.loadImage('img/tiles/start_down.png'),
-      P5.p5.loadImage('img/tiles/start_right.png'),
-      P5.p5.loadImage('img/tiles/start_left.png'),
-      P5.p5.loadImage('img/tiles/start_up.png'),
-      P5.p5.loadImage('img/tiles/crystal.png'),
+      P5.p5.loadImage('../img/tiles/orange.png'),
+      P5.p5.loadImage('../img/tiles/black.png'),
+      P5.p5.loadImage('../img/tiles/end_down.png'),
+      P5.p5.loadImage('../img/tiles/end_right.png'),
+      P5.p5.loadImage('../img/tiles/end_left.png'),
+      P5.p5.loadImage('../img/tiles/end_up.png'),
+      P5.p5.loadImage('../img/tiles/start_down.png'),
+      P5.p5.loadImage('../img/tiles/start_right.png'),
+      P5.p5.loadImage('../img/tiles/start_left.png'),
+      P5.p5.loadImage('../img/tiles/start_up.png'),
+      P5.p5.loadImage('../img/tiles/crystal.png'),
     ]
   }
 
   static hudImages() {
     return [
-      P5.p5.loadImage('img/hud/normal.png'),
-      P5.p5.loadImage('img/hud/upgrading.png'),
-      P5.p5.loadImage('img/hud/upgrading_max.png'),
+      P5.p5.loadImage('../img/hud/normal.png'),
+      P5.p5.loadImage('../img/hud/upgrading.png'),
+      P5.p5.loadImage('../img/hud/upgrading_max.png'),
     ]
   }
 
   static hudIconImages() {
     return [
-      P5.p5.loadImage('img/hud/icon_green_tower_on.png'),
-      P5.p5.loadImage('img/hud/icon_green_tower_off.png'),
-      P5.p5.loadImage('img/hud/icon_red_tower_on.png'),
-      P5.p5.loadImage('img/hud/icon_red_tower_off.png'),
-      P5.p5.loadImage('img/hud/icon_yellow_tower_on.png'),
-      P5.p5.loadImage('img/hud/icon_yellow_tower_off.png'),
+      P5.p5.loadImage('../img/hud/icon_green_tower_on.png'),
+      P5.p5.loadImage('../img/hud/icon_green_tower_off.png'),
+      P5.p5.loadImage('../img/hud/icon_red_tower_on.png'),
+      P5.p5.loadImage('../img/hud/icon_red_tower_off.png'),
+      P5.p5.loadImage('../img/hud/icon_yellow_tower_on.png'),
+      P5.p5.loadImage('../img/hud/icon_yellow_tower_off.png'),
     ]
   }
 
   static backgroundImage() {
-    return P5.p5.loadImage('img/backgrounds/ground.jpg')
+    return P5.p5.loadImage('../img/backgrounds/ground.jpg')
   }
 
   static magicFireball() {
-    return P5.p5.loadImage('img/magics/fireball.png')
+    return P5.p5.loadImage('../img/magics/fireball.png')
   }
 
   static magicIceball() {
-    return P5.p5.loadImage('img/magics/iceball.png')
+    return P5.p5.loadImage('../img/magics/iceball.png')
   }
 
   static magicUFO() {
-    return P5.p5.loadImage('img/magics/ufo.png')
+    return P5.p5.loadImage('../img/magics/ufo.png')
   }
 }
