060604833e4a797551e0adc9a5cf44b01e49995d
darellanodev <darellano.aliaga@gmail.com>
2024-04-25 20:15:30 +0200

    Flyindicator move up and set the text content

--- src/hud/FlyIndicator.ts
+++ src/hud/FlyIndicator.ts
@@ -1,23 +1,38 @@
+import { P5 } from '../utils/P5'
 import { Position } from '../utils/types'
 
 export class FlyIndicator {
   static MAX_TIME_ALIVE: number = 100
+  static MOVE_INCREMENT: number = 1
 
-  #initialPosition: Position
+  #position: Position
   #isActive: boolean = true
   #aliveTime: number = 0
-  constructor(position: Position) {
-    this.#initialPosition = { ...position }
+  #text: string = ''
+  constructor(position: Position, text: string) {
+    this.#position = { ...position }
+    this.#text = text
   }
 
   get isActive() {
     return this.#isActive
   }
 
+  get position(): Position {
+    return this.#position
+  }
+
+  draw() {
+    P5.p5.text(this.#text, this.#position.x, this.#position.y)
+  }
+
   update() {
     this.#aliveTime++
     if (this.#aliveTime > FlyIndicator.MAX_TIME_ALIVE) {
       this.#isActive = false
+    } else {
+      const newY = this.#position.y - FlyIndicator.MOVE_INCREMENT
+      this.#position = { x: this.#position.x, y: newY }
     }
   }
 }
--- test/FlyIndicator.test.ts
+++ test/FlyIndicator.test.ts
@@ -1,11 +1,13 @@
 import { FlyIndicator } from '../src/hud/FlyIndicator'
 import { Position } from '../src/utils/types'
 
+const flyIndicatorText: string = ''
+
 describe('isActive', () => {
   test('when FlyIndicator is recently created, return true', () => {
     const initialPosition: Position = { x: 100, y: 200 }
 
-    const flyIndicator = new FlyIndicator(initialPosition)
+    const flyIndicator = new FlyIndicator(initialPosition, flyIndicatorText)
 
     const result = flyIndicator.isActive
 
@@ -14,7 +16,7 @@ describe('isActive', () => {
   test('when FlyIndicator is recently created and updated MAX_TIME_ALIVE + 1 times, return false', () => {
     const initialPosition: Position = { x: 100, y: 200 }
 
-    const flyIndicator = new FlyIndicator(initialPosition)
+    const flyIndicator = new FlyIndicator(initialPosition, flyIndicatorText)
     const updateTimes = FlyIndicator.MAX_TIME_ALIVE + 1
     for (let i = 0; i < updateTimes; i++) {
       flyIndicator.update()
@@ -23,4 +25,16 @@ describe('isActive', () => {
     const result = flyIndicator.isActive
     expect(result).toBeFalsy()
   })
+})
+
+test('get position, when FlyIndicator is recently created and updated one time, position.y is < than initial position.y', () => {
+  const initialPosition: Position = { x: 100, y: 200 }
+
+  const flyIndicator = new FlyIndicator(initialPosition, flyIndicatorText)
+  flyIndicator.update()
+
+  const position = flyIndicator.position
+
+  const result = position.y < initialPosition.y
+  expect(result).toBeTruthy()
 })
