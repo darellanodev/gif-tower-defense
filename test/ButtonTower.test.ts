3ce74cd0a9f32a8f5c79f93b8aa8982e0b8064d0
darellanodev <darellano.aliaga@gmail.com>
2024-05-19 11:46:39 +0200

    format

--- test/ButtonTower.test.ts
+++ test/ButtonTower.test.ts
@@ -6,8 +6,8 @@ test('isChecked, if clicked and the last status was unchecked, return true', () 
   const buttonPosition: Position = { x: 100, y: 100 }
   const buttonSize: Size = { w: 100, h: 200 }
   const buttonTower = new ButtonTower(buttonPosition, buttonSize, images)
-
   buttonTower.check()
+
   const result = buttonTower.isChecked
 
   expect(result).toBeTruthy()
@@ -16,9 +16,9 @@ test('isChecked, if clicked and the last status was unchecked and then uncheck t
   const buttonPosition: Position = { x: 100, y: 100 }
   const buttonSize: Size = { w: 100, h: 200 }
   const buttonTower = new ButtonTower(buttonPosition, buttonSize, images)
-
   buttonTower.check()
   buttonTower.uncheck()
+
   const result = buttonTower.isChecked
 
   expect(result).toBeFalsy()
