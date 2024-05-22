ef3b76661111aa1c083c6236133ecccf130577e1
darellanodev <darellano.aliaga@gmail.com>
2024-05-22 21:38:00 +0200

    remove unnecesary code

--- src/enemies/EnemyInstances.ts
+++ src/enemies/EnemyInstances.ts
@@ -4,24 +4,6 @@ import { Player } from '../player/Player'
 import { Enemy } from './Enemy'
 
 export class EnemyInstances {
-  static VELOCITY = 1 // must be multiple of "this.#Const.TILE_SIZE". Set 1 for normal, 5 for a faster game or 25 for a fastest game
-  static BOSS_VELOCITY = 0.5
-  static INDEX_BOSS_IN_ENEMIES_IMAGES = 5
-  static CHANGE_EYES_MAX_TIME = 50
-  static EXTEND_CLOSED_EYES_MAX_TIME = 20
-  static MIN_TIME_TO_CLOSE = 50
-  static MAX_TIME_TO_CLOSE = 200
-  static EYES_CENTER = 0
-  static EYES_LEFT = 1
-  static EYES_RIGHT = 2
-  static EYES_CLOSED = 3
-  static STATUS_ALIVE = 0
-  static STATUS_DEAD = 1
-  static TOTAL_ENEMIES = 5
-  static CREATION_MAX_TIME = 200 // 100 when ENEMY_VELOCITY is 1. Decrement it if you speed up the game.
-  static SIZE = 50
-  static REDUCTION_FACTOR = 0.6
-
   static numberOfEnemies = 0 // for generating IDs
   static instances: Enemy[] = []
   static waveEnemies: number = 0
