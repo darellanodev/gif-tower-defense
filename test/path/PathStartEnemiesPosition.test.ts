87b0f1ae7d4d1b8b3084acc809140ca7d501dad4
darellanodev <darellano.aliaga@gmail.com>
2024-07-29 21:42:49 +0200

    tests for starting enemies position in level 15

--- test/path/PathStartEnemiesPosition.test.ts
+++ test/path/PathStartEnemiesPosition.test.ts
@@ -92,3 +92,37 @@ test('getEnemiesInitialPosition, levelMap 13 (double loop) when start direction 
   }
   expect(result).toMatchObject(expected)
 })
+
+test('getEnemiesInitialPosition, levelMap 15 (psycho), return {x:800, y:80}', () => {
+  const topRightTileY = 80
+  const topRightTileX = 750
+
+  const tileStartPosition = {
+    x: topRightTileX - Const.TILE_SIZE * 7,
+    y: topRightTileY + Const.TILE_SIZE * 9,
+  }
+  const levelId = 15
+  const levelMap = getLevelMap(levelId)
+
+  const tilesManager = new TilesManager()
+  TileStartCreator.clearInstance()
+  const tileStartCreator = getTileStartCreator()
+
+  tileStartCreator.create(levelMap, tilesManager)
+  const tileStart = tilesManager.tileStart
+
+  if (tileStart === null) {
+    throw new Error('Error tileStart is null')
+  }
+
+  const pathStartEnemyPosition = PathStartEnemiesPosition.getInstance()
+  pathStartEnemyPosition.tileStart = tileStart
+
+  const result = pathStartEnemyPosition.get()
+
+  const expected = {
+    x: tileStartPosition.x,
+    y: tileStartPosition.y + Const.TILE_SIZE,
+  }
+  expect(result).toMatchObject(expected)
+})
