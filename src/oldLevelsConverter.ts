6d8a4b56f80ffb49ce85ae2ea5ba66177e2cfc76
darellanodev <darellano.aliaga@gmail.com>
2025-03-05 20:11:17 +0100

    Add the prefix processed to the old processed levels (#10)

--- src/oldLevelsConverter.ts
+++ src/oldLevelsConverter.ts
@@ -17,19 +17,28 @@ function convertOldFormatLevels() {
   })
 
   const convertedLevels: string[] = []
+  const finalOldLevels: string[] = []
   for (const oldLevel of oldLevels) {
+    let finalOldLevel = oldLevel
     const oldLevelConverter = new OldLevelConverter.OldLevelConverter(oldLevel)
     if (!oldLevelConverter.canConvert(availableTiles)) {
+      finalOldLevels.push(finalOldLevel)
       continue
     }
     if (oldLevelConverter.existsLevelId(AllLevels.data, oldLevel)) {
+      finalOldLevels.push(finalOldLevel)
       continue
     }
+    finalOldLevels.push(`***processed***${finalOldLevel}`)
     convertedLevels.push(oldLevelConverter.json)
   }
   fs.writeFileSync(
     './src/levels/levelsData/oldLevelsConverted.json',
     convertedLevels.join('\n'),
+  )
+  fs.writeFileSync(
+    './src/levels/levelsData/oldLevels.txt',
+    finalOldLevels.join('\n'),
   )
 }
 
