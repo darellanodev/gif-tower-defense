b59eb8a18f5595b4c01e5d57da7153b2bd6543be
darellanodev <darellano.aliaga@gmail.com>
2025-06-19 21:58:16 +0200

    refactor: Call the parent’s draw method first in the draw method of ButtonMagic (#32)

--- src/hud/ButtonMagic.ts
+++ src/hud/ButtonMagic.ts
@@ -45,15 +45,7 @@ export class ButtonMagic extends Button {
   }
 
   draw() {
-    if (this.#items > 0) {
-      if (this.isMouseOver({ x: P5.p5.mouseX, y: P5.p5.mouseY })) {
-        this.drawOnHover()
-      } else {
-        this.drawOn()
-      }
-    } else {
-      this.drawOff()
-    }
+    super.draw()
     P5.p5.text(
       this.#items,
       this.position.x + this.#offsetItems.x,
