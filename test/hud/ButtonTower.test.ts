6abca8d2aa7dd0b64d46b28939fb9387c805a1ae
darellanodev <darellano.aliaga@gmail.com>
2025-07-10 21:45:21 +0200

    test: Fix class to instantiate a Button in ButtonTower tests (#32)

--- test/hud/ButtonTower.test.ts
+++ test/hud/ButtonTower.test.ts
@@ -3,25 +3,26 @@ import { ButtonTower } from '../../src/hud/ButtonTower'
 import { Position } from '../../src/types/position'
 import { Size } from '../../src/types/size'
 import { images } from '../helpers/imagesResources'
+import { Button } from '../../src/hud/Button'
 
 test('isChecked, if clicked and the last status was unchecked, return true', () => {
   const buttonPosition: Position = { x: 100, y: 100 }
   const buttonSize: Size = { w: 100, h: 200 }
-  const buttonTower = new ButtonTower(buttonPosition, buttonSize, images)
+  const buttonTower = new Button(buttonPosition, buttonSize, images)
   buttonTower.check()
 
-  const result = buttonTower.isChecked
+  const result = buttonTower.checked
 
   expect(result).toBeTruthy()
 })
 test('isChecked, if clicked and the last status was unchecked and then uncheck the button, return false', () => {
   const buttonPosition: Position = { x: 100, y: 100 }
   const buttonSize: Size = { w: 100, h: 200 }
-  const buttonTower = new ButtonTower(buttonPosition, buttonSize, images)
+  const buttonTower = new Button(buttonPosition, buttonSize, images)
   buttonTower.check()
   buttonTower.uncheck()
 
-  const result = buttonTower.isChecked
+  const result = buttonTower.checked
 
   expect(result).toBeFalsy()
 })
