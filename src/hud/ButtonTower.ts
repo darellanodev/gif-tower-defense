a6dc3a3e3b1c3441a8a509c233d5cd27cff1aa15
darellanodev <darellano.aliaga@gmail.com>
2025-07-10 21:43:47 +0200

    refactor: Remove unused code (#32)

--- src/hud/ButtonTower.ts
+++ src/hud/ButtonTower.ts
@@ -1,8 +1,5 @@
 import { Image } from 'p5'
 import { Button } from './Button'
-import { Position } from '../types/position'
-import { Size } from '../types/size'
-import { P5 } from '../utils/P5'
 import { ButtonGreenTowerCreator } from './ButtonGreenTowerCreator'
 import { ButtonRedTowerCreator } from './ButtonRedTowerCreator'
 import { ButtonYellowTowerCreator } from './ButtonYellowTowerCreator'
@@ -16,70 +13,6 @@ export class ButtonTower extends Button {
   static towerRedButtonImages: Image[]
   static towerYellowButtonImages: Image[]
 
-  static INDEX_IMAGE_OFF_HOVER = 3
-
-  #isChecked: boolean = false
-  #isOn: boolean = false
-
-  check() {
-    this.#isChecked = true
-  }
-  uncheck() {
-    this.#isChecked = false
-  }
-  get isChecked() {
-    return this.#isChecked
-  }
-  drawCheckRectangle() {
-    P5.p5.strokeWeight(3)
-    P5.p5.stroke(255, 204, 0)
-    P5.p5.noFill()
-    P5.p5.square(
-      this.position.x + this.offsetImages.x - 2,
-      this.position.y + this.offsetImages.y - 2,
-      36,
-    )
-  }
-
-  drawOffHover() {
-    P5.p5.image(
-      this.images[ButtonTower.INDEX_IMAGE_OFF_HOVER],
-      this.position.x + this.offsetImages.x,
-      this.position.y + this.offsetImages.y,
-    )
-  }
-
-  get isMouseInsideAndNotChecked(): boolean {
-    return (
-      this.isMouseOver({ x: P5.p5.mouseX, y: P5.p5.mouseY }) && !this.#isChecked
-    )
-  }
-
-  #drawOn() {
-    if (this.isMouseInsideAndNotChecked) {
-      this.drawOnHover()
-    } else {
-      this.drawOn()
-    }
-  }
-  #drawOff() {
-    if (this.isMouseInsideAndNotChecked) {
-      this.drawOffHover()
-    } else {
-      this.drawOff()
-    }
-  }
-
-  draw() {
-    if (this.#isOn) {
-      this.#drawOn()
-    } else {
-      this.#drawOff()
-    }
-    if (this.#isChecked) {
-      this.drawCheckRectangle()
-    }
-  }
   static setImages(
     towerGreenButtonImages: Image[],
     towerRedButtonImages: Image[],
