b4c4f55be0d908d911df8f8637eb6b047fcf8887
darellanodev <darellano.aliaga@gmail.com>
2025-12-01 18:11:02 +0100

    refactor: Extract checkCollision private method in MagicIceballCollisionChecker class

--- src/magics/MagicIceballCollisionChecker.ts
+++ src/magics/MagicIceballCollisionChecker.ts
@@ -14,17 +14,22 @@ export class MagicIceballCollisionChecker {
     this.#magicCollisionChecker = magicCollisionChecker
     this.#pathMovement = pathMovement
   }
+
+  #checkCollision(magicIceball: MagicIceball, enemy: Enemy) {
+    if (
+      this.#magicCollisionChecker.checkCollision(
+        enemy,
+        this.#pathMovement.indexOrder,
+      )
+    ) {
+      this.handleCollision(magicIceball, enemy)
+    }
+  }
+
   check(magicIceball: MagicIceball, enemies: Enemy[]) {
     enemies.forEach((enemy) => {
       if (!enemy.isAbducted) {
-        if (
-          this.#magicCollisionChecker.checkCollision(
-            enemy,
-            this.#pathMovement.indexOrder,
-          )
-        ) {
-          this.handleCollision(magicIceball, enemy)
-        }
+        this.#checkCollision(magicIceball, enemy)
       }
     })
   }
